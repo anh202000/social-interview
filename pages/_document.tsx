4cb9b5244138486cd7d363474040223781e50fe5
Le Phan tuan Anh <anhroyal110@gmail.com>
2023-03-10 22:09:58 +0700

    feature/fix error str_mode jq+boostrap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
       </Head>
       <body>
         <Main />
